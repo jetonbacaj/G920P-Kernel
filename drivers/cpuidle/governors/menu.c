44caa5d5251c599e333608c9f7a1a6fdfc6fadb8
Andrei F <luxneb@gmail.com>
2015-07-30 21:40:18 +0200

    cpuidle: menu: Add permissions and optimize perfect C-state residency

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -109,6 +109,13 @@
  *
  */
 
+/*
+ * The C-state residency is so long that is is worthwhile to exit
+ * from the shallow C-state and re-enter into a deeper C-state.
+ */
+static unsigned int perfect_cstate_ms __read_mostly = 10;
+module_param(perfect_cstate_ms, uint, 0644);
+
 struct menu_device {
 	int		last_state_idx;
 	int             needs_update;
