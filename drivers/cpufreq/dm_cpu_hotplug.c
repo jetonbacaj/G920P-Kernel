d7c38fa00e00ca77e6c085f90e4a77514abb5616
Lukas0610 <mail@lukasberger.at>
2017-05-01 01:34:56 +0200

    cpufreq: removed flooding dm_hotplug message

--- drivers/cpufreq/dm_cpu_hotplug.c
+++ drivers/cpufreq/dm_cpu_hotplug.c
@@ -1091,9 +1091,6 @@ static int on_run(void *data)
 				exe_cmd = diagnose_condition();
 
 		if (exynos_dm_hotplug_disabled()) {
-#ifdef DM_HOTPLUG_DEBUG
-			pr_info("dm_hotplug disable = %d\n", exynos_dm_hotplug_disabled());
-#endif
 			goto sleep;
 		}
 
