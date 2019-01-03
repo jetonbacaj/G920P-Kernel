18b41354b4534dc41a860d9bce6e87d25a73260e
Jeton Bacaj <jbacaj@ocrolus.com>
2019-01-02 22:32:09 -0500

    Add build scripts and ramdisk

---
+++ build_kernel.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+###############################################################################
+# To all DEV around the world :)                                              #
+# to build this kernel you need to be ROOT and to have bash as script loader  #
+# do this:                                                                    #
+# cd /bin                                                                     #
+# rm -f sh                                                                    #
+# ln -s bash sh                                                               #
+#                                                                             #
+# Now you can build my kernel.                                                #
+# using bash will make your life easy. so it's best that way.                 #
+# Have fun and update me if something nice can be added to my source.	      #
+#                                                         		      #
+# Original scripts by halaszk & various sources throughout gitHub             #
+# modified by UpInTheAir for SkyHigh kernels				      #
+# very very slightly modified by The Sickness for his Twisted S6 kernel       #
+#                                                         		      #
+###############################################################################
+
+
+############################################ SETUP ############################################
+
+# Time of build startup
+res1=$(date +%s.%N)
+#################################### OPTIONAL SOURCE CLEAN ####################################
+echo
+echo "${bldcya}***** Setting up Environment *****${txtrst}";
+echo
+. ./env_setup.sh ${1} || exit 1;
+
+if [ ! -f $KERNELDIR/.config ]; then
+	echo
+	echo "${bldcya}***** Writing Config *****${txtrst}";
+	cp $KERNELDIR/arch/arm64/configs/$KERNEL_CONFIG .config;
+	make ARCH=arm64 $KERNEL_CONFIG;
+fi;
+
+. $KERNELDIR/.config
+
+
+########################################### CLEAN UP ##########################################
+
+echo
+echo "${bldcya}***** Clean up first *****${txtrst}"
+
+find . -type f -name "*~" -exec rm -f {} \;
+find . -type f -name "*orig" -exec rm -f {} \;
+find . -type f -name "*rej" -exec rm -f {} \;
+
+if [ -e $BK/$TARGET/boot.img ]; then
+	rm -rf $BK/$TARGET/boot.img
+fi;
+if [ -e $BK/$TARGET/Image ]; then
+	rm -rf $BK/$TARGET/Image
+fi;
+
+echo "Done"
+
+####################################### COMPILE IMAGES #######################################
+cd ${KERNELDIR}
+echo
+echo "${bldcya}***** Compiling kernel *****${txtrst}"
+
+if [ $USER != "root" ]; then
+	make CONFIG_DEBUG_SECTION_MISMATCH=y -j8 Image ARCH=arm64
+else
+	make -j8 Image ARCH=arm64
+fi;
+
+if [ -e $KERNELDIR/arch/arm64/boot/Image ]; then
+	echo
+	echo "${bldcya}***** Final Touch for Kernel *****${txtrst}"
+	stat $KERNELDIR/arch/arm64/boot/Image || exit 1;
+	mv ./arch/arm64/boot/Image ./$BK/$TARGET
+	echo
+else
+	echo "${bldred}Kernel STUCK in BUILD!${txtrst}"
+	exit 0;
+fi;
+
+echo
+echo "Done"
+
+############ BOOT.IMG GENERATION #####################################
+
+echo
+echo "${bldcya}***** Make boot.img *****${txtrst}"
+echo
+cd ${KERNELDIR}/$BK
+./mkbootimg --kernel ./$TARGET/Image --dt ./$TARGET/dt.img --ramdisk ./$TARGET/ramdisk.gz --base 0x10000000 --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --pagesize 2048 -o ./$TARGET/boot.img
+
+echo -n "SEANDROIDENFORCE" >> ./$TARGET/boot.img
+
+echo "Done"
+
+#################################### OPTIONAL SOURCE CLEAN ####################################
+
+echo
+echo "${bldcya}***** Clean source *****${txtrst}"
+
+cd ${KERNELDIR}
+read -p "Do you want to Clean the source? (y/n) > " mc
+if [ "$mc" = "Y" -o "$mc" = "y" ]; then
+	make clean
+	make mrproper
+fi
+
+echo
+echo "Build completed"
+echo
+echo "${txtbld}***** Flashable zip found in output directory *****${txtrst}"
+echo
+# build script ends
---
+++ env_setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Colorize and add text parameters
+export red=$(tput setaf 1)             #  red
+export grn=$(tput setaf 2)             #  green
+export blu=$(tput setaf 4)             #  blue
+export cya=$(tput setaf 6)             #  cyan
+export txtbld=$(tput bold)             #  Bold
+export bldred=${txtbld}$(tput setaf 1) #  red
+export bldgrn=${txtbld}$(tput setaf 2) #  green
+export bldblu=${txtbld}$(tput setaf 4) #  blue
+export bldcya=${txtbld}$(tput setaf 6) #  cyan
+export txtrst=$(tput sgr0)             #  Reset
+
+
+# check if ccache installed, if not install
+if [ ! -e /usr/bin/ccache ]; then
+	echo "You must install 'ccache' to continue.";
+	sudo apt-get install ccache
+fi
+
+# check if xmllint installed, if not install
+if [ ! -e /usr/bin/xmllint ]; then
+	echo "You must install 'xmllint' to continue.";
+	sudo apt-get install libxml2-utils
+fi
+
+echo "${bldcya}***** Clean up Environment before compile *****${txtrst}";
+
+
+# Make clean source
+read -t 5 -p "Make clean source, 5sec timeout (y/n)?";
+if [ "$REPLY" == "y" ]; then
+make distclean;
+make mrproper;
+fi;
+
+# clear ccache
+read -t 5 -p "Clear ccache but keeping the config file, 5sec timeout (y/n)?";
+if [ "$REPLY" == "y" ]; then
+ccache -C;
+fi;
+
+
+TARGET='G920P'
+echo
+echo "Starting your build for $TARGET"
+
+# location
+export KERNELDIR=`readlink -f .`;
+
+
+# set build variables
+BK=build_kernel
+export KCONFIG_NOTIMESTAMP=true
+export ARCH=arm64;
+export SUB_ARCH=arm64;
+
+export KERNEL_CONFIG="my_defconfig";
+
+# build script
+export USER=`whoami`;
+export TMPFILE=`mktemp -t`;
+
+
+# system compiler
+export CROSS_COMPILE=/home/jeton/Downloads/kernel/toolchain/android-ndk-r11c/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
+
+# CPU Core
+export NUMBEROFCPUS=`grep 'processor' /proc/cpuinfo | wc -l`;
